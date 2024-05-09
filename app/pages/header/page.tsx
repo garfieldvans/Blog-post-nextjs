0274ab16c9d321590341c65402b8406b8595a69d
garfieldvans <iyanhalo@gmail.com>
2024-05-09 16:43:51 +0700

    [feat] create button on header logo

--- app/pages/header/page.tsx
+++ app/pages/header/page.tsx
@@ -17,7 +17,12 @@ const Header: React.FC = () => {
   return (
     <header className="sticky inset-x-0 inset-y-0 py-4 md:px-18 px-10 flex items-center bg-yellow-300 drop-shadow-lg Containers z-40">
       <div className=" mr-6 ">
-        <h2 className="text-black text-md md:text-lg font-bold logo">NEXT Admin</h2>
+        <h2 className="text-black text-md md:text-lg font-bold logo">
+        <Link
+          href="/"
+        >
+          NEXT Admin
+        </Link></h2>
       </div>
       <div className=" font-medium text-base py-2 hidden md:flex md:flex-row ">
         <Link
